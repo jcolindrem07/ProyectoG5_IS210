f9eaf9994c81a689fa31c18ea733fd82bdc0261d
jcolindrem07 <85214058+jcolindrem07@users.noreply.github.com>
2021-08-07 18:14:34 -0600

    Dato enum NombreVacuna
    Dato para los nombres de las vacunas con solo 5 valores posibles PFIZER, MODERNA, SPUTNIK, ASTRAZENECA y JOHNSON

---
+++ NombreVacuna.java
@@ -0,0 +1,7 @@
+/**
+ * Tipo de dato enum para almacenar el nombre de las vacunas
+ * @author Joseph A. Colindres
+ */
+public enum NombreVacuna {
+    PFIZER, MODERNA, SPUTNIK, ASTRAZENECA, JOHNSON;
+}
